7d563a4ab7d2f960357b0ecf98a94b767fa4682d
Hemendra Mahajan <hemmhjnacts87@gmail.com>
2025-11-13 05:43:32 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
+  "projectId": "studio-7943908738-8bbf8",
+  "appId": "1:392878434867:web:78bbe0731e745654be1146",
   "apiKey": "AIzaSyDOC7T94WuuLw3j2isTaoDAvUmDMrCslyQ",
   "authDomain": "studio-7943908738-8bbf8.firebaseapp.com",
-  "projectId": "studio-7943908738-8bbf8",
-  "storageBucket": "studio-7943908738-8bbf8.appspot.com",
-  "messagingSenderId": "392878434867",
-  "appId": "1:392878434867:web:1f9911f4579644d4be1146",
-  "measurementId": ""
+  "measurementId": "",
+  "messagingSenderId": "392878434867"
 };
